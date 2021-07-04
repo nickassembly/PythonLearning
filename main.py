65f7c6d76e73a4a26cff5a1fde3ac34f01826503
nickguerra <nickg@maliciousgames.com>
2021-07-04 08:03:47 -0500

    added variables to script

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello Python")
+calculation_to_units = 24
+name_of_unit = "hours"
+
+print(f"20 days are {20 * calculation_to_units} {name_of_unit}")
+print(f"35 days are {35 * calculation_to_units} {name_of_unit}")
+print(f"50 days are {25 * calculation_to_units} {name_of_unit}")
+print(f"110 days are {110 * calculation_to_units} {name_of_unit}")
+
+
