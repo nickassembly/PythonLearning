29f701f07cf0c81915629ac51b34c627177b1f2c
nickguerra <nickg@maliciousgames.com>
2021-07-17 06:08:27 -0500

    added new method with def keyword

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-calculation_to_units = 24
+calculation_to_hours = 24
 name_of_unit = "hours"
 
-print(f"20 days are {20 * calculation_to_units} {name_of_unit}")
-print(f"35 days are {35 * calculation_to_units} {name_of_unit}")
-print(f"50 days are {25 * calculation_to_units} {name_of_unit}")
-print(f"110 days are {110 * calculation_to_units} {name_of_unit}")
+
+def days_to_units(num_of_days, custom_message):
+    print(f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}")
+    print(custom_message)
+
+days_to_units(20, "Great!")
+days_to_units(10, "Meh")
+
 
 
