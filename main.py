146c51e914869bedcb4512f746829802fe4ec8bb
nickguerra <nickg@maliciousgames.com>
2021-08-07 08:34:38 -0500

    Practice various scope types with examples

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 calculation_to_hours = 24
 name_of_unit = "hours"
 
-
 def days_to_units(num_of_days, custom_message):
     print(f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}")
     print(custom_message)
-
-days_to_units(20, "Great!")
-days_to_units(10, "Meh")
-
-
 
