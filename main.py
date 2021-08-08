e3c0d0e50d8c861b9f10d408a912079f2fc79772
nickguerra <nickg@maliciousgames.com>
2021-08-08 08:19:37 -0500

    experimenting with Input built in function

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 calculation_to_hours = 24
 name_of_unit = "hours"
 
-def days_to_units(num_of_days, custom_message):
-    print(f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}")
-    print(custom_message)
+def days_to_units(num_of_days):
+    return f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}"
+
+user_input = input("Enter a number of days to convert to hours.\n")
+print(user_input)
+
 
