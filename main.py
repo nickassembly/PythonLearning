5adc746fd76b86e6feef71a2b232cae57075761d
nickguerra <nickg@maliciousgames.com>
2021-08-14 07:22:48 -0500

    Exercise with casting and conditionals

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ calculation_to_hours = 24
 name_of_unit = "hours"
 
 def days_to_units(num_of_days):
-    return f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}"
+    if num_of_days > 0:
+        return f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}"
 
 user_input = input("Enter a number of days to convert to hours.\n")
-print(user_input)
+user_input_number = int(user_input)
 
+calculated_value = days_to_units(user_input_number)
+print(calculated_value)
 
