1d7263344eea64607890190c38165ec2da8fe4a0
nickguerra <nickg@maliciousgames.com>
2021-09-10 11:37:32 -0500

    More practice with conditionals

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ calculation_to_hours = 24
 name_of_unit = "hours"
 
 def days_to_units(num_of_days):
+    condition_check = num_of_days > 0
+    print(type(condition_check))
+
     if num_of_days > 0:
         return f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}"
+    elif num_of_days == 0:
+        return "You entered 0, please enter a valid positive number"
+    else:
+        return "You entered a negative value, no conversion available"
 
 user_input = input("Enter a number of days to convert to hours.\n")
 user_input_number = int(user_input)
