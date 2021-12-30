7bf3589a39a031943644025a2eeb3aae56ef0d93
nickguerra <nickg@maliciousgames.com>
2021-12-30 13:52:52 -0600

    modified program to accept dictionary data types

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-calculation_to_hours = 24
-name_of_unit = "hours"
 
-def days_to_units(num_of_days):
-    return f"{num_of_days} days are {num_of_days * calculation_to_hours} {name_of_unit}"
+def days_to_units(num_of_days, conversion_unit):
+    if conversion_unit == "hours":
+      return f"{num_of_days} days are {num_of_days * 24} hours"
+    elif conversion_unit == "minutes":
+      return f"{num_of_days} days are {num_of_days * 24 * 60} minutes"
+    else:
+      return "unsupported unit"
 
 def validate_and_execute():
         try:
-            user_input_number = int(num_of_days_element)
-            # we want to do conversion only for positive ints
+            user_input_number = int(days_and_unit_dictionary["days"])
             if user_input_number > 0:
-              calculated_value = days_to_units(user_input_number)
+              calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
               print(calculated_value)
             elif user_input_number == 0:
               print("You entered 0, please enter a valid positive number")
@@ -23,8 +25,11 @@ user_input = ""
 while user_input != "exit":
     user_input = input("Enter number of days and conversion unit.\n")
     days_and_unit = user_input.split(":")
-
-
+    print(days_and_unit)
+    days_and_unit_dictionary = {"days": days_and_unit[0], "unit":days_and_unit[1]}
+    print(days_and_unit_dictionary)
+    print(type(days_and_unit_dictionary))
+    validate_and_execute()
 
 
 
