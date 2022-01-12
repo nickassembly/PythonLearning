2e679ab878eab3ed11a7ad2c5f7a8e6704b66362
nickguerra <nickg@maliciousgames.com>
2022-01-12 05:40:34 -0600

    Added loop to iterate through excel sheets

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import openpyxl
 
+inv_file = openpyxl.load_workbook("inventory.xlsx")
+product_list = inv_file["Sheet1"]
 
+products_per_supplier = {}
+
+print(product_list.max_row)
+
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4)
 
 
 
