5622a3bbb8fd0a88f00ab95252aec5321c57716d
nickguerra <nickg@maliciousgames.com>
2022-01-13 05:17:04 -0600

    Added logic to fill dictionary

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ product_list = inv_file["Sheet1"]
 
 products_per_supplier = {}
 
-print(product_list.max_row)
-
 for product_row in range(2, product_list.max_row + 1):
-    supplier_name = product_list.cell(product_row, 4)
+    supplier_name = product_list.cell(product_row, 4).value
+    print(supplier_name)
 
+    if supplier_name in products_per_supplier:
+        current_num_products = products_per_supplier.get(supplier_name)
+        products_per_supplier[supplier_name] = current_num_products + 1
+    else:
+        products_per_supplier[supplier_name] = 1
+
+print(products_per_supplier)
 
 
 
